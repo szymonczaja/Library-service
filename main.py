94a0cdc65394def10874426bed44c9c61df5e76e
Szymon <szymonczaja2003@gmail.com>
2026-04-25 16:15:03 +0200

    Add db to .gitignore file, new demo in main

--- main.py
+++ main.py
@@ -1,29 +1,26 @@
-from models import Book, Loan, Member
-from repositories import LoanRepository, BookRepository, MemberRepository
-from service import LibraryService
+import uuid
 from datetime import date
+from database import db_init
+from sql_repositories import SQLiteBookRepository, SQLiteMemberRepository, SQLiteLoanRepository
+from service import LibraryService
+from models import Book, Member
 
-def demo():
-    book1 = Book('123231', 'zycie', 'Szymon', 2100)
-    book2 = Book('we1234', 'ciepienie', 'Bogdan', 2200)
-    member1 = Member('adasdasd', 'Kris', 'kris@gmail.com')
-    member2 = Member('afanfafn', 'Woj', 'woj@wp.pl')
-    member_repo = MemberRepository()
-    book_repo = BookRepository()
-    loan_repo = LoanRepository()
-    library = LibraryService(loan_repo, member_repo, book_repo)
-    book_repo.add(book1)
-    book_repo.add(book2)
-    member_repo.add(member1)
-    member_repo.add(member2)
-    loan = library.checkout_by_ids('123231', 'adasdasd', date(2026, 4, 1), date(2026, 4, 10))
-    print("Wypożyczono:", loan)
-    print("\nPrzeterminowane:")
-    for l in library.get_overdue_loans(date.today()):
-        print(l)
+conn = db_init('library_database.db')
 
-    print("\nHistoria Krisa:")
-    for l in library.member_history('adasdasd'):
-        print(l)
+book_repo = SQLiteBookRepository(conn)
+member_repo = SQLiteMemberRepository(conn)
+loan_repo = SQLiteLoanRepository(conn)
 
-demo()
+service = LibraryService(loan_repo, member_repo, book_repo)
+
+book = Book(str(uuid.uuid4()), 'Wiedźmin', 'Andrzej Sapkowski', 1990)
+member = Member(str(uuid.uuid4()), 'Jan Kowalski', 'jan@kowalski.pl')
+
+book_repo.add(book)
+member_repo.add(member)
+
+loan = service.checkout(book, member, date.today(), date(2026, 5, 25))
+print(f'Wypożyczono: {loan}')
+
+stats = service.get_statistics(date.today())
+print(f'Statystyki: {stats}')
